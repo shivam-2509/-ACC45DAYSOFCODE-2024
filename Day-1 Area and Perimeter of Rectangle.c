910b9664f4e4e79bf008aeb4ceaa663733e87b69
shivam-2509 <agrawalshivam5167@gmail.com>
2024-09-29 12:53:23 +0530

    Add files via upload

---
+++ Day-1 Area and Perimeter of Rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+ int main()
+ {
+   int l=10,b=8,rectangle;
+    printf("area of rectangle is:%d",l*b);
+    printf("\n perimeter of rectangle is :%d",2*(l+b));
+    
+    return 0;
+ }
